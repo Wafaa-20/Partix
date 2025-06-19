8a0c3bc491a4af4f00c7b005e25996e810f40d0c
lojain <lojain089@gmail.com>
2025-06-19 22:19:13 +0300

    start2

--- partix/lib/app.dart
+++ partix/lib/app.dart
@@ -8,9 +8,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: AppTheme.theme,
-      routerConfig: router,
-    );
+    return MaterialApp.router(theme: AppTheme.theme, routerConfig: router);
   }
 }
+////00
