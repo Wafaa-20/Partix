4615b78b5b4dbf3e3647be97ae81b30317223f22
ahmedAlmansour99 <almansour.ahmed99@gmail.com>
2025-06-26 11:51:32 +0300

    fix some bugs

--- partix/lib/main.dart
+++ partix/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 
-=======
 import 'package:geolocator/geolocator.dart';
->>>>>>> 7e586c6f35f805f76aa6314b480c890d2c6b2356
 import 'app.dart';
 import 'core/dependency_injection/setup.dart';
 
@@ -13,8 +10,6 @@ void main() async {
 <<<<<<< HEAD
 =======
   LocationPermission permission = await Geolocator.checkPermission();
-
-  if (permission == LocationPermission.denied ||
       permission == LocationPermission.deniedForever) {
     permission = await Geolocator.requestPermission();
   }
--- partix/windows/flutter/generated_plugin_registrant.cc
+++ partix/windows/flutter/generated_plugin_registrant.cc
@@ -7,20 +7,14 @@
 #include "generated_plugin_registrant.h"
 
 #include <app_links/app_links_plugin_c_api.h>
-<<<<<<< HEAD
-=======
 #include <geolocator_windows/geolocator_windows.h>
->>>>>>> 7e586c6f35f805f76aa6314b480c890d2c6b2356
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AppLinksPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AppLinksPluginCApi"));
-<<<<<<< HEAD
-=======
   GeolocatorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("GeolocatorWindows"));
->>>>>>> 7e586c6f35f805f76aa6314b480c890d2c6b2356
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
